91d2ee540efcd2adad5a76edaac89d070cd48447
Paing Kaung Sett <kaungsettpaing@haj-emp.co.jp>
2020-12-29 17:17:01 +0900

    change last column hover style

--- public/js/customer.js
+++ public/js/customer.js
@@ -29,7 +29,7 @@ $(document).ready(function() {
     "columnDefs": [{
         // "targets": -2,
         "orderable": false,
-        width: '4.5%',
+        // width: '4.5%',
         "sClass": 'options'
     }]
   });
@@ -39,7 +39,7 @@ $(document).ready(function() {
   $("#dataTable tr td").mouseenter(function () {
     trIndex = $(this).parent();
     $(trIndex).find("td:last-child")
-      .html(`<a href="#"><i class="far fa-edit"></i></a>
+      .html(`<span>${salary}</span>&nbsp;&nbsp;<a href="#"><i class="far fa-edit"></i></a>
         &nbsp;&nbsp;
         <a href="#"><i class="far fa-trash-alt"></i></a>
         &nbsp;&nbsp;
@@ -48,18 +48,7 @@ $(document).ready(function() {
   });
 
   // remove button on tr mouseleave
-
   $("#dataTable tr td").mouseleave(function () {
     $(trIndex).find('td:last-child').html(`${salary}`);
   });
-  // $("#dataTable tr").hover(
-  //   function(){
-  //     $(this).append(
-  //       $("<span><a href='#'><i class='far fa-edit'></i></a>&nbsp;&nbsp;<a href='#'><i class='far fa-trash-alt'></i></a></span>")
-  //     );
-  //   },
-  //   function(){
-  //     $(this).find("span").last().remove();
-  //   }
-  // );
 })
