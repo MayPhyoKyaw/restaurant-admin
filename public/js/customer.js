da140c595eca8615a7452a2fd5131a8826afe000
Paing Kaung Sett <kaungsettpaing@haj-emp.co.jp>
2021-01-05 12:00:38 +0900

    test

--- public/js/customer.js
+++ public/js/customer.js
@@ -1,12 +1,62 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
   // create data
-  $("#create_button, #detail_button, #edit_button, #delete_button").click(function() {
+  $("#create_button, #detail_button, #edit_button, #delete_button, #detail_btn").click(function () {
     var buttonValue = $(this).attr("value")
     console.log(buttonValue)
-      $(".heading").text(`${buttonValue} Customer`);
-      // $(".submit-button").text(`${buttonValue}`);
-      $(".submit-button").html(`<i class="fas fa-check fa-fw"></i>${buttonValue}`);
+    $(".heading").text(`${buttonValue} Customer`);
+    $(".submit-button").html(`<i class="fas fa-check fa-fw"></i>${buttonValue}`);
+
+    // if (buttonValue === "Create") {
+    //   $(".submit-button").click(function () {
+    //     console.log("Yah")
+    //     // var { MongoClient } = require("mongodb");
+    //     var dbKey = require("../route/database/dangerousKey.json");
+    //     console.log(dbKey)
+
+    //     // // Replace the following with your Atlas connection string
+    //     // var url = dbKey.key;
+    //     // var client = new MongoClient(url);
+
+    //     // // The database to use
+    //     // var dbName = "test-kk";
+
+    //     // async function run() {
+    //     //   try {
+    //     //     await client.connect();
+    //     //     console.log("Connected correctly to server");
+    //     //     var db = client.db(dbName);
+    //     //     // Use the collection "people"
+    //     //     var col = db.collection("people");
+    //     //     // Construct a document
+    //     //     var personDocument = {
+    //     //       "docid": "_id",
+    //     //       "name": { "first": "KaungSett", "last": "Paing" },
+    //     //       "birth": new Date(1912, 5, 23), // June 23, 191
+    //     //       "work": ["IT", "translate", "Engineer"],
+    //     //       "views": 1250000
+    //     //     }
+    //     //     // Insert a single document, wait for promise so we can read it back
+    //     //     var p = await col.insertOne(personDocument);
+    //     //     // Find one document
+    //     //     var myDoc = await col.findOne();
+    //     //     // Print to the console
+    //     //     console.log(myDoc);
+    //     //   } catch (err) {
+    //     //     console.log(err.stack);
+    //     //   }
+
+    //     //   finally {
+    //     //     await client.close();
+    //     //   }
+    //     // }
+    //     // run().catch(console.dir);
+    //   })
+    // }
+  })
+
+  $(".detail_btn").click(function () {
+    console.log("Yahh")
   })
 
   var myTable = $('#dataTable').DataTable({
@@ -34,7 +84,7 @@ $(document).ready(function() {
         "sClass": 'options',
       },
       {
-        "targets": [0,1],
+        "targets": [0, 1],
         "orderable": false,
         "width": "1%",
       },
@@ -44,7 +94,7 @@ $(document).ready(function() {
       // style: 'os',
       selector: 'td:first-child'
     },
-    order: [[ 2, 'asc' ]]
+    order: [[2, 'asc']]
   });
 
   // $('#dataTable tbody').on('click', 'tr', function () {
@@ -65,12 +115,11 @@ $(document).ready(function() {
       .html(`
         <span>${salary}</span>
         &nbsp;&nbsp;
-        <a href="#"><span class="detail-icon"><i class="far fa-eye fa-fw"></i></span></a>
+        <a href="#" data-toggle="modal" data-target="#customerModal"><span class="detail-icon"><i class="far fa-eye fa-fw"></i></span></a>
         &nbsp;&nbsp;
-        <a href="#"><span class="edit-icon"><i class="far fa-edit fa-fw"></i></span></a>
+        <a href="#" data-toggle="modal" data-target="#customerModal"><span class="edit-icon"><i class="far fa-edit fa-fw"></i></span></a>
         &nbsp;&nbsp;
-        <a href="#"><span class="delete-icon"><i class="far fa-trash-alt fa-fw"></i></span></a>
-
+        <a href="#" data-toggle="modal" data-target="#customerModal"><span class="delete-icon"><i class="far fa-trash-alt fa-fw"></i></span></a>
       `);
   });
 
@@ -88,7 +137,7 @@ $(document).ready(function() {
   }
 
   // close cancel button to reset modal
-  $("#customerModal").on("hidden.bs.modal", function() {
+  $("#customerModal").on("hidden.bs.modal", function () {
     document.getElementById("customer_form").reset();
   })
 
